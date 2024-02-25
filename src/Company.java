8e717b9f94bdc49a57defd216d863a3cd9acc8e5
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2024-02-25 21:53:10 +0000

    Starting Company

--- src/Company.java
+++ src/Company.java
@@ -1,9 +1,35 @@
+import java.util.ArrayList;
 
 
-/**
- *
- * @author jamie
- */
+
+
 public class Company {
+  
+    private String companyName;
+    private ArrayList <Employee> staff;
+    
+     
+//Constructors    
+    public Company() {
+        this.companyName = "Random Company Name";
+        this.staff = new ArrayList<>();
+    }
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+        this.staff = new ArrayList<>();
+    }
+
+//Methods
+      public void addNewStaff(Employee employee) {
+        staff.add(employee);
+    }
+      
+       public int getStaffNumber() {
+        return staff.size();
+    }
+    
     
 }
+
+
