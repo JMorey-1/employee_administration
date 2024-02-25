11f26634201d39bb212cb8b9a54506f15519c529
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2024-02-25 21:35:52 +0000

    Working on Part 1

--- src/Employee.java
+++ src/Employee.java
@@ -1,12 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author jamie
- */
+
+
+
 public class Employee {
     
+//Instance fields
+    private String name;
+    private String email;
+    private int empNum;
+    
+//Static fields
+    private static int nextEmpNum = 1;
+
+
+//Constructors
+    
+    //Default value constructor
+    public Employee() {
+        this.name = "John Doe";
+        this.email = "johndoe@example.com";
+        this.empNum = nextEmpNum++;        
+    } 
+    
+    //Parameter constructor
+    public Employee(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.empNum = nextEmpNum++;
+        
+    }
+
+//Methods
+    
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getEmpNum(){
+        return empNum;
+    }
+    
+    public static int getNextEmpNum(){
+        return nextEmpNum;
+    }
+
+    public void setEmail(String email) {
+        if (email.length() > 3) {
+            this.email = email;
+          
+        } else {
+            System.out.println("Please enter valid email!");
+        }
+    }
+
+
 }
+
+
+
+
--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -1,13 +1,45 @@
 
 
-/**
- *
- * @author jamie
- */
 public class EmployeeTest {
     
     
      public static void main(String[] args) {
-     }
+    
+         //Creating employee objects
+         Employee employee1 = new Employee ("Joe Bloggs", "jb@gmail.com");
+         Employee employee2 = new Employee ("Ann Banana", "ab@gmail.com");
+         Employee employee3 = new Employee ("Tom Thumb", "tt@gmail.com");
+        
+         
+           //Delcaring array and storing employee objects
+            Employee[] projectGroup = {employee1, employee2, employee3};
      
-}
+           // Print the value of variable nextEmpNum
+            System.out.println("Value of nextEmpNum: " + Employee.getNextEmpNum());
+     
+          //Declaring variable "m" 
+            int m = 2;
+     
+             System.out.println("Employees with empNum above " + m + ":");
+              for (Employee employee : projectGroup) {
+                if (employee.getEmpNum() > m) {
+                 System.out.println(employee.getName());
+                }
+              }
+     }    
+   
+
+        //Email validation
+       
+              
+              
+  }
+
+     
+     
+     
+     
+     
+     
+     
+
